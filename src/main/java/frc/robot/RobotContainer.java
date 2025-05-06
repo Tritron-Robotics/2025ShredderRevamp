c23248c70a829443d37e3ff3f35235fb182ac726
Zahir Roblox <128651180+ZahirCR7@users.noreply.github.com>
2025-05-06 12:25:33 -0700

    changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -96,7 +96,6 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     // Ex: m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-    m_driverController.b().whileTrue(/*do this later */)
   }
 
   /**
